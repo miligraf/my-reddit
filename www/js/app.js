96dc4f7d03249f0e27636002109df99bb939c545
Milton Andrade <miligraf@gmail.com>
2016-07-14 22:23:50 -0400

    infinite scroll and pull to refresh

--- www/js/app.js
+++ www/js/app.js
@@ -6,11 +6,34 @@ app.controller('RedditCtrl', function($http, $scope) {
 
   $scope.stories = [];
 
-  $http.get('http://www.reddit.com/r/StarWarsBattlefront/new.json').success(function(response) {
-    angular.forEach(response.data.children, function(child) {
-      $scope.stories.push(child.data);
+  function loadStories(params, callback) {
+    $http.get('http://www.reddit.com/r/StarWarsBattlefront/new.json', {params: params}).success(function(response) {
+      var stories = [];
+      angular.forEach(response.data.children, function(child) {
+        stories.push(child.data);
+      });
+      callback(stories);
     });
-  });
+  }
+
+  $scope.loadOlderStories = function() {
+    var params = {};
+    if ($scope.stories.length > 0) {
+      params['after'] = $scope.stories[$scope.stories.length - 1].name;
+    }
+    loadStories(params, function(olderStories) {
+      $scope.stories = $scope.stories.concat(olderStories);
+      $scope.$broadcast('scroll.infiniteScrollComplete');
+    });
+  };
+
+  $scope.loadNewerStories = function() {
+    var params = {'before': $scope.stories[0].name};
+    loadStories(params, function(newerStories) {
+      $scope.stories = newerStories.concat($scope.stories);
+      $scope.$broadcast('scroll.refreshComplete');
+    });
+  };
 
 });
 
